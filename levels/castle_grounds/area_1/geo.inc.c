e45f48de89bb0fe3a2db0851c867cc8267628ec1
thecozies <theredcouchroom@gmail.com>
2021-03-08 08:37:25 -0600

    Level updates for testing

--- levels/castle_grounds/area_1/geo.inc.c
+++ levels/castle_grounds/area_1/geo.inc.c
@@ -4,6 +4,7 @@ const GeoLayout castle_grounds_area_1_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, castle_grounds_dl_Cube_001_mesh),
+		GEO_TRANSLATE_NODE_WITH_DL(5, 0, -380, 0, castle_grounds_dl_Plane_mesh_layer_5),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
 };
@@ -30,6 +31,7 @@ const GeoLayout castle_grounds_area_1[] = {
 			GEO_CLOSE_NODE(),
 		GEO_CLOSE_NODE(),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, castle_grounds_dl_material_revert_render_settings),
+		GEO_DISPLAY_LIST(5, castle_grounds_dl_material_revert_render_settings),
 	GEO_CLOSE_NODE(),
 	GEO_END(),
 };
---
+++ levels/castle_grounds/texscroll.inc.h
@@ -0,0 +1,3 @@
+extern void scroll_castle_grounds_dl_Plane_mesh_layer_5_vtx_0();
+extern void scroll_sts_mat_castle_grounds_dl_water_layer5_area1();
+extern void scroll_castle_grounds();
