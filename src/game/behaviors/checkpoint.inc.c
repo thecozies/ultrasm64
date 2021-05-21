92cb25630021ee3978e59bd1ba20e7cbef940cf5
thecozies <collinpferguson@gmail.com>
2021-05-21 05:34:03 -0500

    QoL includes

--- src/game/behaviors/checkpoint.inc.c
+++ src/game/behaviors/checkpoint.inc.c
@@ -1,4 +1,15 @@
-// warp.c.inc
+#ifndef o
+#include "game/level_update.h"
+#include "game/object_helpers.h"
+#include "game/object_list_processor.h"
+#include "game/rendering_graph_node.h"
+#include "game/puppycam2.h"
+#include "game/area.h"
+#include "game/mario.h"
+#include "object_fields.h"
+
+#define o gCurrentObject
+#endif
 
 void bhv_checkpoint_loop(void) {
     if (o->oDistanceToMario < 500.0f) {
