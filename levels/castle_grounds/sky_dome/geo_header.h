190d14df66028218dbfaf006e93c3a2f916e9163
thecozies <collinpferguson@gmail.com>
2021-05-31 05:18:33 -0500

    improved sky dome

--- levels/castle_grounds/sky_dome/geo_header.h
+++ levels/castle_grounds/sky_dome/geo_header.h
@@ -1,7 +1,7 @@
 extern const GeoLayout sky_dome_geo[];
 extern u8 sky_dome_stars_custom_i8[];
 extern Vtx sky_dome_sky_dome_001_mesh_layer_0_vtx_cull[8];
-extern Vtx sky_dome_sky_dome_001_mesh_layer_0_vtx_0[1284];
+extern Vtx sky_dome_sky_dome_001_mesh_layer_0_vtx_0[381];
 extern Gfx sky_dome_sky_dome_001_mesh_layer_0_tri_0[];
 extern Gfx mat_sky_dome_big_sky_dome_mat[];
 extern Gfx mat_revert_sky_dome_big_sky_dome_mat[];
