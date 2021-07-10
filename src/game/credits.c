9cec64e0d322f9c31d5e1fdf7adf8c4fd5ab2ea2
thecozies <collinpferguson@gmail.com>
2021-07-10 16:54:01 -0500

    Updated credits text and timing

--- src/game/credits.c
+++ src/game/credits.c
@@ -45,7 +45,7 @@ s32 sNumThanks[4] = { AIR_THANKS, WATER_THANKS, CAMERA_THANKS, EXTRA_THANKS };
 char sLucys[] = "Lucy's";
 char sLevitation[] = "Levitation";
 
-char sTitleGame[] = "Modeling | Music | Programming";
+char sTitleGame[] = "Modeling | Programming";
 char sGameText[] = "C | The Cozies";
 
 char sTitleTextures[] = "Textures";
@@ -59,41 +59,25 @@ char sMusicText[][22] = {
 };
 
 char sTitleCodingHelp[] = "Coding Help";
-char sCodingHelpText[][16] = {
-    "Wiseguy",
-    "Fazana",
-    "someone2639",
-    "axollyon",
-    "Kaze",
-    "Reonu",
-    "anonymous_moose",
-    "CrashOveride",
-    "devwizard",
-    "InTheBeef",
-    "gamemasterplc",
-    "Arthurtilly",
+char sCodingHelpText[][52] = {
+    "Wiseguy\nFazana\nsomeone2639\naxollyon",
+    "Kaze Emanuar\nReonu\nanonymous_moose\nCrashOveride",
+    "devwizard\nInTheBeef\ngamemasterplc\nArthurtilly",
 };
 
 char sTitleTools[] = "Tools | Engines";
-char sToolsText[][34] = {
-    "Fazana | Puppycam",
-    "someone2639 | S2D Text Engine",
-    "kurethedead | Fast64",
-    "CrashOveride | ultrasm64",
-    "Arthurtilly | sm64-audio-manager",
-    "Buu342 | UNFLoader"
+char sToolsText[][54] = {
+    "Fazana | Puppycam\nsomeone2639 | S2D Text Engine",
+    "kurethedead | Fast64\nCrashOveride | ultrasm64",
+    "Buu342 | UNFLoader\nArthurtilly | sm64-audio-manager"
 };
 
 char sTitleTesters[] = "Testing";
-char sTestersText[][20] = {
-    "rocky | fordescort",
-    "Reonu",
-    "The Sour OG"
-};
+char sTestersText[] = "rocky | fordescort\nReonu\nThe Sour OG";
 
 char sTitleSpecial[] = "Special thanks to...";
-char sMessageSimple[] = "Simpleflips - for expanding the SM64 hacking community.";
-char sMessageSimple2[] = "From players to modders, we couldn't have gone this far without you.";
+char sMessageSimple[] =  "Simpleflips - for expanding the SM64\nhacking community.";
+char sMessageSimple2[] = "From players to modders, we couldn't\nhave gone this far without you.";
 
 char sMessageJams[] = "Axollyon and Wiseguy";
 char sMessageJams2[] = "For organizing and creating Mario Jams.";
@@ -175,21 +159,28 @@ s32 handle_fade(f32 attack, s32 sustain, f32 release, struct CreditState *credit
 }
 
 #define C_ATTACK 30.0f
-#define C_SUSTAIN 150
-#define C_SUSTAIN_LONG 200
-#define C_SUSTAIN_SHORT 100
-#define C_SUSTAIN_VERY_SHORT 60
+#define C_SUSTAIN_XLONG 150
+#define C_SUSTAIN_LONG 100
+#define C_SUSTAIN 60
+#define C_SUSTAIN_SHORT 45
+#define C_SUSTAIN_VERY_SHORT 30
+// #define C_SUSTAIN 150
+// #define C_SUSTAIN_LONG 200
+// #define C_SUSTAIN_SHORT 100
+// #define C_SUSTAIN_VERY_SHORT 60
 #define C_RELEASE 20.0f
 
 #define CENTER_W ((SCREEN_WIDTH / 2))
-#define TITLE_OFFSET 8
+#define TITLE_OFFSET 0
 #define TITLE_H 50
 #define TITLE_MID_H ((SCREEN_HEIGHT / 2) - 30)
-#define TITLE_MID_L ((SCREEN_HEIGHT / 2) + 30)
+#define TITLE_MID_L ((SCREEN_HEIGHT / 2))
 
-#define MESSAGE_OFFSET 4
+#define MESSAGE_OFFSET 0
 #define MESSAGE_H 110
 #define MESSAGE_H_2 130
+#define MESSAGE_H_3 150
+#define MESSAGE_H_4 170
 
 #define TITLE_SIZE 1.0f
 #define MESSAGE_SIZE 0.5f
@@ -201,6 +192,7 @@ s32 handle_fade(f32 attack, s32 sustain, f32 release, struct CreditState *credit
 
 #define print_message(s)      (s2d_print_deferred((CENTER_W + MESSAGE_OFFSET), MESSAGE_H,    ALIGN_CENTER, sCreditMessageState.fadeAlpha, MESSAGE_SIZE, s))
 #define print_message_low(s)  (s2d_print_deferred((CENTER_W + MESSAGE_OFFSET), MESSAGE_H_2,  ALIGN_CENTER, sCreditMessageState.fadeAlpha, MESSAGE_SIZE, s))
+#define print_message_lower(s)  (s2d_print_deferred((CENTER_W + MESSAGE_OFFSET), MESSAGE_H_3,  ALIGN_CENTER, sCreditMessageState.fadeAlpha, MESSAGE_SIZE, s))
 #define print_message_hold(s) (s2d_print_deferred((CENTER_W + MESSAGE_OFFSET), MESSAGE_H,    ALIGN_CENTER, 255,                           MESSAGE_SIZE, s))
 
 void reset_credit_state(struct CreditState *creditState) {
@@ -225,7 +217,7 @@ void next_title(void) {
 void print_titles(void) {
     if (sCreditTitleState.section == CREDITS_END) return;
 
-    s32 done = handle_fade(C_ATTACK, sCreditTitleState.section == CREDITS_GAME_TITLE ? C_SUSTAIN_LONG : 0, C_RELEASE, &sCreditTitleState);
+    s32 done = handle_fade(C_ATTACK, sCreditTitleState.section == CREDITS_GAME_TITLE ? C_SUSTAIN_XLONG : 0, C_RELEASE, &sCreditTitleState);
     if (done) next_title();
 
     switch(sCreditTitleState.section) {
@@ -279,25 +271,20 @@ void print_messages(void) {
         case CREDITS_MUSIC:
             print_message(sMusicText[sCreditMessageState.section]);
             lastMessage = 1;
-            sustain = C_SUSTAIN_SHORT;
+            // sustain = C_SUSTAIN_SHORT;
             break;
         case CREDITS_CODING:
             print_message(sCodingHelpText[sCreditMessageState.section]);
             // lastMessage = LAST_INDEX(sCodingHelpText);
-            lastMessage = 11;
-            sustain = C_SUSTAIN_VERY_SHORT;
+            lastMessage = 2;
             break;
         case CREDITS_TOOLS:
             print_message(sToolsText[sCreditMessageState.section]);
             // lastMessage = LAST_INDEX(sToolsText);
-            lastMessage = 5;
-            sustain = C_SUSTAIN_SHORT;
+            lastMessage = 2;
             break;
         case CREDITS_TESTING:
-            print_message(sTestersText[sCreditMessageState.section]);
-            // lastMessage = LAST_INDEX(sTestersText);
-            lastMessage = 2;
-            sustain = C_SUSTAIN_SHORT;
+            print_message(sTestersText);
             break;
         case CREDITS_SPECIAL:
             if (sCreditMessageState.section == 0) {
@@ -305,7 +292,7 @@ void print_messages(void) {
                     if (sCreditMessageState.fadeState >= FADING_OUT) print_message(sMessageSimple);
                     else print_message_hold(sMessageSimple);
 
-                    print_message_low(sMessageSimple2);
+                    print_message_lower(sMessageSimple2);
                     if (handle_fade(C_ATTACK, C_SUSTAIN_LONG, C_RELEASE, &sCreditMessageState)) {
                         sCreditMessageState.section++;
                         holdForFollowup = FALSE;
